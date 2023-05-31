d83a3748bcf680c8e1c23f5d58622aa89227b1ce
Gabriel Franco <grocha815@gmail.com>
2023-05-31 09:36:36 -0300

    Criei a navbar e a estilizei.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,95 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Container, Row, Col, Nav } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
+import "../../styles/header.css";
+import logoImg from "../../assets/all-images/Logo/logo.png";
+
+const navLinks_left = [
+  {
+    path: "/consulta",
+    display: "Consulta",
+  },
+  {
+    path: "/vacinas",
+    display: "Vacinas",
+  },
+
+  {
+    path: "/exames",
+    display: "Exames",
+  },
+];
+
+const navLinks_right = [
+  {
+    path: "/especialidades",
+    display: "Especialidades",
+  },
+  {
+    path: "/contato",
+    display: "Contato",
+  },
+  {
+    path: "/sobre",
+    display: "Sobre",
+  },
+];
 
 const Header = () => {
-  return <div></div>;
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => menuRef.current.classList.toggle("menu_active");
+
+  return (
+    /* ======== naveção principal ========= */
+    <header>
+      <div className="main_navbar">
+        <Container>
+          <div className="navigation_wrapper d-flex align-items-center justify-content-between">
+            <span className="mobile_menu">
+              <i className="ri-menu-fill" onClick={toggleMenu}></i>
+            </span>
+
+            <div className="navigation_left">
+              <div className="menu_left">
+                {navLinks_left.map((item, index) => (
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive ? "nav_active nav_item" : "nav_item"
+                    }
+                    key={index}
+                  >
+                    {item.display}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+
+            <Link to="/home" className="logo-link">
+              <img src={logoImg} className="logo" alt="Logo" />
+            </Link>
+
+            <div className="navigation_right">
+              <div className="menu_right">
+                {navLinks_right.map((item, index) => (
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive ? "nav_active nav_item" : "nav_item"
+                    }
+                    key={index}
+                  >
+                    {item.display}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
