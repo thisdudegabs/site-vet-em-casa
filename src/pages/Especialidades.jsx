b2e5f1418d545c8172dd1f4d21c1706bae4e1bdf
Gabriel Franco <grocha815@gmail.com>
2023-06-13 23:02:59 -0300

    Mais sessões adicionadas, mais informações e bugs

--- src/pages/Especialidades.jsx
+++ src/pages/Especialidades.jsx
@@ -5,8 +5,8 @@ import Helmet from "../components/Helmet/Helmet";
 import "../styles/especialidades.css";
 import "../styles/card-time.css";
 
-import ImgEspeci from "../assets/all-images/Animais/dog4.jpg";
 import CardTime from "../components/UI/CardTime";
+import SectionEspecialidades from "../components/UI/SectionEspecialidades";
 
 import TrabalheConosco from "../components/UI/TrabalheConosco";
 
@@ -30,44 +30,8 @@ const Especialidades = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="6" md="6">
-              <div className="section_especialidades-conteudo">
-                <h2 className="section_title"> As nossas Especialidades! </h2>
-                <p className="description">
-                  Incididunt enim ea sit commodo. Enim anim non reprehenderit
-                  minim laborum cupidatat. Reprehenderit voluptate reprehenderit
-                  tempor tempor pariatur eu. Qui culpa Lorem reprehenderit culpa
-                  occaecat ut. In commodo minim non adipisicing pariatur
-                  deserunt eiusmod do commodo consequat tempor culpa. Deserunt
-                  mollit deserunt labore aliqua laborum. Culpa commodo culpa
-                  excepteur ut consequat dolor amet magna adipisicing.
-                  Reprehenderit fugiat dolore ex esse dolor reprehenderit esse.
-                  Id tempor veniam consectetur aute dolor adipisicing laborum.
-                  Eiusmod cupidatat deserunt ipsum labore enim anim cupidatat
-                  proident amet esse eiusmod velit excepteur. Incididunt enim ea
-                  sit commodo.
-                </p>
-
-                <button className="btn especialidades_btn mt-4">
-                  <Link to="/contato">Quero Viver Mais Com Meu Pet!</Link>
-                </button>
-              </div>
-            </Col>
-
-            <Col lg="6" md="6">
-              <div className="especialidades_img">
-                <img src={ImgEspeci} alt="" className="w-100" />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-
-      <section>
-        <Container>
-          <Row>
             <Col lg="12" className="mb-5 text-center">
-              <h6 className="section_subtitle">Especialistas</h6>
+              <h6 className="section_subtitle custom_top">Especialistas</h6>
               <h2 className="section_title">Nosso Time</h2>
               <CardTime showCardTime={showCardTime} />
             </Col>
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,7 +1,133 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/sobre.css";
+import "../assets/all-images/Fillers/wavebg1.svg";
 
 const Sobre = () => {
-  return <div></div>;
+  return (
+    <Helmet>
+      <section>
+        <Container>
+          <Row>
+            <Col>
+              <section>
+                <div>
+                  <h1>NÓS AMAMOS O QUE FAZEMOS</h1>
+                  <p>
+                    Nosso excelente time de veterinários, fisioterapeutas,
+                    acupunturistas, cardiologistas e de mais diversas
+                    especialidades, se dedica para o bem estar e conforto do seu
+                    pet, atendendo diretamente da sua casa!
+                  </p>
+                </div>
+              </section>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <div className="curva-bg">
+          <div className="curva-cima">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path
+                fill="#987ea4"
+                fill-opacity="10"
+                d="M0,192L60,176C120,160,240,128,360,133.3C480,139,600,181,720,192C840,203,960,181,1080,165.3C1200,149,1320,139,1380,133.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+              ></path>
+            </svg>
+          </div>
+
+          <div className="curva-baixo">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path
+                fill="#987ea4"
+                fill-opacity="10"
+                d="M0,192L60,176C120,160,240,128,360,133.3C480,139,600,181,720,192C840,203,960,181,1080,165.3C1200,149,1320,139,1380,133.3L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="galeria">
+          <div>
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto1.png"
+                alt="Foto Veterinário Com o Pet 1"
+              />
+            </span>
+
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto2.png"
+                alt="Foto Veterinário Com o Pet 2"
+              />
+            </span>
+
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto3.png"
+                alt="Foto Veterinário Com o Pet 3"
+              />
+            </span>
+
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto4.png"
+                alt="Foto Veterinário Com o Pet 4"
+              />
+            </span>
+
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto5.png"
+                alt="Foto Veterinário Com o Pet 5"
+              />
+            </span>
+
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto6.png"
+                alt="Foto Veterinário Com o Cachorro"
+              />
+            </span>
+
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto7.png"
+                alt="Foto Veterinário Com o Cachorro"
+              />
+            </span>
+
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto8.jpeg"
+                alt="Foto Veterinário Com o Cachorro"
+              />
+            </span>
+
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto9.jpeg"
+                alt="Foto Veterinário Com o Cachorro"
+              />
+            </span>
+
+            <span>
+              <img
+                src="../assets/all-images/Galeria/foto10.png"
+                alt="Foto Veterinário Com o Cachorro"
+              />
+            </span>
+          </div>
+        </div>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Sobre;
